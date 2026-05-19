2dd1dd59c49e8cd48eb82606ab41f9add8188453
Izk-123 <109396886+Izk-123@users.noreply.github.com>
2026-05-19 23:38:47 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('projects.urls')),
     path('api/skills/', include('skills.urls')),
+    path('api/content/', include('content.urls')),
 ]
